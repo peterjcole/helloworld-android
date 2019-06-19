602877d0d68283dc6eecd2cbb10c64b0281c72be
Peter Cole <11048765+peterjcole@users.noreply.github.com>
2019-06-19 16:55:22 +0100

    Hello world!

---
+++ app/src/main/java/net/petercole/helloworld/MainActivity.java
@@ -0,0 +1,28 @@
+package net.petercole.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "net.petercole.helloworld.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    //called when user taps send button
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
