bd561370b7d718c0331a52441e1e20c05e687081
Peter Cole <11048765+peterjcole@users.noreply.github.com>
2019-07-19 10:57:38 +0100

    add modified files

--- app/src/main/java/net/petercole/helloworld/DisplayMessageActivity.java
+++ app/src/main/java/net/petercole/helloworld/DisplayMessageActivity.java
@@ -12,6 +12,8 @@ import android.widget.TextView;
 public class DisplayMessageActivity extends AppCompatActivity {
 
     public static String PRESET_MESSAGE = "net.petercole.helloworld.PRESET_MESSAGE";
+    public static String EMAIL_SUBJECT = "A message from Peter's hello world";
+    String message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +21,7 @@ public class DisplayMessageActivity extends AppCompatActivity {
         setContentView(R.layout.activity_display_message);
 
         Intent intent = getIntent();
-        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         String colour = intent.getStringExtra(MainActivity.RADIO_CHOSEN);
 
         TextView textView = findViewById(R.id.textView);
@@ -36,6 +38,16 @@ public class DisplayMessageActivity extends AppCompatActivity {
         data.putExtra(PRESET_MESSAGE, selectedText);
         setResult(RESULT_OK, data);
         finish();
+    }
+
+    public void shareMessage(View view) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, message);
+        intent.putExtra(Intent.EXTRA_SUBJECT, EMAIL_SUBJECT);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 
 }
